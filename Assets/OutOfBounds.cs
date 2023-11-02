b44fbddfb49bfb0025882b958f64aadeca63b6e0
LBneus <66575803+LBneus@users.noreply.github.com>
2023-11-02 16:01:08 -0500

    adding boundary in multiplayer scene

---
+++ Assets/OutOfBounds.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SoccerBall"))
+        {
+            other.transform.position = new Vector3(0, 0, 0);
+
+            Rigidbody ballRigidbody = other.GetComponent<Rigidbody>();
+            if (ballRigidbody)
+            {
+                ballRigidbody.velocity = Vector3.zero;
+                ballRigidbody.angularVelocity = Vector3.zero;
+            }
+
+        }
+    }
+
+}
