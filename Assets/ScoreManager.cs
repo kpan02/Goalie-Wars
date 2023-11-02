b336256f633bd656725a980ebb03b03ed124e9f2
WilliamStar <89805831+WilliamStar007@users.noreply.github.com>
2023-11-02 12:13:14 -0500

    scoring fixed

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ScoreManager : MonoBehaviourPunCallbacks
+public class ScoreManager : MonoBehaviour
 {
     public TMP_Text scoreOneText; // Player One Score text
     public TMP_Text scoreTwoText; // Player Two Score text
@@ -14,16 +14,24 @@ public class ScoreManager : MonoBehaviourPunCallbacks
     private int scoreOne = 0; // Player One Score counter
     private int scoreTwo = 0; // Player Two Score counter
 
+    private PhotonView photonView;
+    public TMP_Text DebugText;
+
+    void Start()
+    {
+        photonView = this.gameObject.GetComponent<PhotonView>();
+    }
+
     // Call this method to increase the score for Player One.
     public void IncreaseScoreOne()
     {
-        photonView.RPC("RPC_IncreaseScoreOne", RpcTarget.AllViaServer);
+        photonView.RPC("RPC_IncreaseScoreOne", RpcTarget.AllBuffered);
     }
 
     // Call this method to increase the score for Player Two.
     public void IncreaseScoreTwo()
     {
-        photonView.RPC("RPC_IncreaseScoreTwo", RpcTarget.AllViaServer);
+        photonView.RPC("RPC_IncreaseScoreTwo", RpcTarget.AllBuffered);
     }
 
     [PunRPC]
